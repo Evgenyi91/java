b23c964ab7c8b1eeaf161239d1d14951bf920831
Evgeniy <andrej.burakov.91@mail.ru>
2023-04-14 23:28:26 +0300

    dz1

---
+++ dz1.java
@@ -0,0 +1,14 @@
+/**
+ * dz1
+ */
+public class dz1 {
+public static void main(String[] args) {
+    int h = 1000;
+    
+    for(int i = 1; i <= h; i++){
+        System.out.println(i);
+    }
+    
+}
+    
+}
