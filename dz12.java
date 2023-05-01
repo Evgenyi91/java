a6ef9d1f09c781ea4bd07fc7b7a83a5b0f9fecbe
Evgeniy <andrej.burakov.91@mail.ru>
2023-05-01 17:55:17 +0300

    dz12

---
+++ dz12.java
@@ -0,0 +1,28 @@
+
+
+import java.util.LinkedList;
+/**
+ * dz12
+ */
+public class dz12 {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            ll.add(getRandomNumber());
+        }
+        System.out.println(ll);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print(ll.getLast() + ", ");
+            ll.removeLast();
+
+        }
+    }
+
+    public static int getRandomNumber() { // генерирует случайное число
+        double x = (Math.random() * 99);
+        int num = (int) x;
+        return num;
+    }
+}
